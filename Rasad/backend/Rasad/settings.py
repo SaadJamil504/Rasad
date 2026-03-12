09fa2f66f961cbf00d043c7313f4996269694721
Saad Jamil <saadjamil504@gmail.com>
2026-03-12 13:35:06 +0500

    .

--- Rasad/backend/Rasad/settings.py
+++ Rasad/backend/Rasad/settings.py
@@ -51,6 +51,10 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',  # <--- Add this
+    'django.contrib.sessions.middleware.SessionMiddleware'
 ]
 ...
 CORS_ALLOWED_ORIGINS = [
