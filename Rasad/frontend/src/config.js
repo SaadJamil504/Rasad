4e8691bbd58c8d84a2ec5ac8fc4017f6c367b648
saadjamil503 <saadjamil503@gmail.com>
2026-04-17 20:13:10 +0500

    .

--- Rasad/frontend/src/config.js
+++ Rasad/frontend/src/config.js
@@ -1,6 +1,6 @@
 export const config = {
   // Backend API base URL (frontend -> backend).
-  API_URL: 'http://localhost:8000/api',
+  API_URL: 'https://rasad-production-a567.up.railway.app/',
   // Frontend URL
-  FRONTEND_URL: 'http://localhost:5173',
+  FRONTEND_URL: 'https://rasad-ten.vercel.app/',
 };
